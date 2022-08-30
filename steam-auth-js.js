daf268fd2f37fa4318d2ecba0b969741b6f6098b
Sparkle <zanjie1999@yeah.net>
2022-08-30 22:53:08 +0800

    必须await

--- steam-auth-js.js
+++ steam-auth-js.js
@@ -1,15 +1,13 @@
 
-function getSteamAuthCode(secret) {
+async function getSteamAuthCode(secret) {
     let secretU8 = Uint8Array.from(atob(secret), c => c.charCodeAt(0));
 
     // 时间
     let timeIndexU32 = new DataView(new ArrayBuffer(8));
     timeIndexU32.setUint32(4, Math.floor(Date.now() / 30000), false);
 
-    let algo = {name: "HMAC", hash: "SHA-1"};
-    let key = crypto.subtle.importKey('raw', secretU8, algo, false, ["sign"]);
-    // 报错 TypeError: Failed to execute 'sign' on 'SubtleCrypto': parameter 2 is not of type 'CryptoKey'.
-    let hmac = crypto.subtle.sign('HMAC', key, timeIndexU32);
+    let key = await crypto.subtle.importKey('raw', secretU8, {name: "HMAC", hash: "SHA-1"}, false, ["sign"]);
+    let hmac = await crypto.subtle.sign('HMAC', key, timeIndexU32);
     hmac = new DataView(hmac);
 
     let start = hmac.getUint8(19) & 0x0F;
